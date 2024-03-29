5d4da9b84bfc9767ca6f226e9bac9277e5ef6939
Maedot Amha <maedotamha@gmail.com>
2024-03-29 20:28:55 +0300

    the camlink and screen sharing page

--- Front CamLink/src/Pages/CamLinkPage.jsx
+++ Front CamLink/src/Pages/CamLinkPage.jsx
@@ -8,10 +8,19 @@ function CamLinkPage() {
       <div>
       <NavBar />
       </div>
-
-      <div>
-        <h1 className='text-white font-bold text-4xl text-center'>Edit the camLInk page</h1>
+      <div className='flex flex-col gap-[5rem] justify-center font-serif'>
+      <div className=' text-white felx flex-col bg-cyan-800 bg-opacity-70 p-4 h-[13rem] w-[50rem] justify-around rounded-2xl'>
+      <label htmlFor='input-link' className=' p-2 text-3xl font-bold'>Join a connection and contribute your camera</label>
+      <div className='pt-[1.5rem] pl-[2rem]'>
+        <input id ='input-link'className='rounded-full bg-teal-200 h-[3rem] w-[28rem]'type ="text" placeholder='  Link code'/>
+        <button className='bg-green-600 rounded-md ml-[2rem] h-[2.5rem] w-[6rem] text-lg'>link</button>
       </div>
+      </div>
+      <div className=' text-white felx flex-col gap-[2rem] bg-cyan-800 bg-opacity-70 p-4 h-[13rem] w-[50rem] justify-around rounded-2xl'>
+        <h1 className='p-2 text-3xl font-bold'>Start a link to connect and manage cameras</h1>
+        <button className='bg-green-600 text-xl text-gray-900 rounded-full mt-[1.5rem] ml-[15rem] h-[3rem] w-[10rem]'>Start a link</button>
+      </div>
+    </div>
     </div>
   )
 }
--- Front CamLink/src/Pages/ScreenSharing.jsx
+++ Front CamLink/src/Pages/ScreenSharing.jsx
@@ -9,9 +9,19 @@ function ScreenSharing() {
       <NavBar />
       </div>
 
-      <div>
-        <h1 className='text-white font-bold text-4xl'>Edit the Screen Sharing Page</h1>
+      <div className='flex flex-col gap-[5rem] justify-center font-serif'>
+      <div className=' text-white felx flex-col bg-cyan-800 bg-opacity-70 p-4 h-[13rem] w-[50rem] justify-around rounded-2xl'>
+      <label htmlFor='input-link' className=' p-2 text-3xl font-bold'>Watch a screen being shared</label>
+      <div className='pt-[1.5rem] pl-[2rem]'>
+        <input id ='input-link'className='rounded-full bg-teal-200 h-[3rem] w-[28rem]'type ="text" placeholder='  Link code'/>
+        <button className='bg-green-600 rounded-md ml-[2rem] h-[2.5rem] w-[6rem] text-lg'>link</button>
       </div>
+      </div>
+      <div className=' text-white felx flex-col gap-[2rem] bg-cyan-800 bg-opacity-70 p-4 h-[13rem] w-[50rem] justify-around rounded-2xl'>
+        <h1 className='p-2 text-3xl font-bold'>Share your screen for others</h1>
+        <button className='bg-green-600 text-xl text-gray-900 rounded-full mt-[1.5rem] ml-[15rem] h-[3rem] w-[10rem]'>Start a link</button>
+      </div>
+    </div>
       
     </div>
   )
