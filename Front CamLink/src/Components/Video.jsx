f2b0b0a7cad6aeeba54a35a512ca7a643a95df4b
Maedot Amha <maedotamha@gmail.com>
2024-04-05 19:44:49 +0300

    add.

---
+++ Front CamLink/src/Components/Video.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const video = () => {
+  return (
+    <div>
+        <video control width ="100%">
+            <source src ="..\assets\recording.mp4" type = "video/mp4"/>
+            </video>
+    </div>
+  )
+}
--- Front CamLink/src/Pages/About.jsx
+++ Front CamLink/src/Pages/About.jsx
@@ -1,29 +1,87 @@
 import React from 'react'
+import EmailIcon from '@mui/icons-material/Email';
+import Face3TwoToneIcon from '@mui/icons-material/Face3TwoTone';
+import Face6TwoToneIcon from '@mui/icons-material/Face6TwoTone';
+import { Stack, TextField } from '@mui/material'
 
 function About() {
   return (
-    <div className=' app flex flex-col justify-between gap-11'>
-      <h1>this is about us page</h1>
-
-      <div>
-
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio debitis perferendis recusandae itaque ipsa sint omnis, libero in aperiam! Aliquid dignissimos perspiciatis porro ipsum. In, fuga quam! Adipisci facilis vero impedit! Commodi quos possimus dolor. Aut unde quia explicabo eum ipsum provident, non, inventore laboriosam repudiandae beatae, nisi incidunt error.</p>
+    <div className=' app flex flex-col justify-between gap-11 ml-[2rem]'>
+      <div className=''>
+        
       </div>
+  <header className='ml-[2rem] text-3xl font-serif'>
+    <h1>About Us</h1>
+  </header>
 
-     <div>
+  <section id="team" className=' ml-[1rem] font-serif bg-blue-200 rounded-lg shadow-lg p-4'>
+    <h2 className='text-bold text-xl italic '>Meet Our Team</h2>
+    <ul className="space-y-4 text-xl">
+      <li>
+        <h3><Face6TwoToneIcon/>  Abenezer Mesfin (React Developer)</h3>
+        <p className='text-base italic'><a href="mailto:abenabu388@gmail.com"><EmailIcon /> abenabu388@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face6TwoToneIcon/>  Anteneh Addisu (React Developer)</h3>
+        <p className='text-base italic'><a href="mailto:anteaddisue20@gmail.com"><EmailIcon /> anteaddisue20@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face6TwoToneIcon/>  Abenezer Mesfin (React Developer)</h3>
+        <p className='text-base italic'><a href="mailto:abenabu388@gmail.com"><EmailIcon /> abenabu388@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face6TwoToneIcon/>  Elias Derbew (React Developer)</h3>
+        <p className='text-base italic'><a href="mailto:eliasderbew1@gmail.com"><EmailIcon /> eliasderbew1@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face3TwoToneIcon/>  Maedot Amha (React Developer)</h3>
+        <p className='text-base italic'><a href="mailto:maedotamha@gmail.com"><EmailIcon /> maedotamha@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face3TwoToneIcon/>  Tsion Getachew (React Developer)</h3>
+        <p className='text-base italic'><a href="mailto:tsiiiarmy@gmail.com"><EmailIcon /> tsiiiarmy@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face6TwoToneIcon/>  Anteneh Getnet (React Developer)</h3>
+        <p className='text-base italic'><a href="mailto:antenehgetnet09@gmail.com"><EmailIcon /> antenehgetnet09@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face6TwoToneIcon/>  Hanif Siraj (Django Developer)</h3>
+        <p className='text-base italic'><a href="mailto:hnfsrj@gmail.com"><EmailIcon /> hnfsrj@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face6TwoToneIcon/>  Sena Mekonin (Django Developer)</h3>
+        <p className='text-base italic'><a href="mailto:senamekonin19@gmail.com"><EmailIcon /> senamekonin19@gmail.com</a></p>
+      </li>
+      <li>
+        <h3><Face3TwoToneIcon/>  Dagmawit Negash (Django Developer)</h3>
+        <p className='text-base italic'><a href="mailto:21dagmawitnegash@gmail.com"> <EmailIcon /> 21dagmawitnegash@gmail.com</a></p>
+      </li>
+    </ul>
+  </section>
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae recusandae, ea exercitationem neque ipsa aperiam. Facilis minus porro id praesentium quisquam. Nemo praesentium quis quia cumque quidem maxime a repellendus quas? Voluptas possimus fugit ad voluptatem! Tenetur placeat nam fugiat porro facere sequi impedit earum. Libero repellendus facilis minus maxime at vero, officiis illum et, officia, perspiciatis quo commodi! Quasi aliquid provident repellendus minima maxime possimus minus eos facilis. Sit necessitatibus natus totam culpa neque fuga soluta voluptatibus eaque minima nihil obcaecati dignissimos nesciunt asperiores recusandae, cupiditate fugit voluptate nostrum sint laudantium a labore. Eos, esse? Nihil quisquam debitis at.</p>
-     </div>
+  <section id="description" >
+    <h2>About CamLink</h2>
+    <p>CamLink is a revolutionary project that aims to transform the way we capture, share, and preserve moments. By providing users with seamless access to their cameras on a website, CamLink empowers individuals to effortlessly capture and save moments that matter. In today's fast-paced world, where certain events and phenomena are fleeting, CamLink ensures that every significant moment is captured and preserved for posterity.</p>
+    <p>CamLink not only enables users to capture moments but also serves as a powerful tool for storing information. Whether it's recording lectures, documenting events, or archiving memories, CamLink provides a secure and accessible platform for storing valuable information.</p>
+  </section>
+  <h1> User Feedback</h1>
+  <Stack direction = 'row' spacing ={4}>
+    <TextField label ='Name' variant = 'standard' color='secondary'/>
+    <TextField label ='Email' variant = 'standard' type ='email' color='secondary'/>
+    <TextField label ='Message' variant ='standard' color='secondary' />
+  </Stack>
+  <button>Send</button>
+          
+  <div className="text-sm text-gray-500 ">
+  <input type="checkbox" name='' id='' />
+  <p> By proceeding, I accept the Terms of Service and Privacy Policy,
+             and consent to receive updates and marketing communications. </p>
+  </div>
+  <footer>
+    <p>&copy; 2024 CamLink. All rights reserved.</p>
+  </footer>
 
-
-<div>
-
-      <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, delectus adipisci id consequatur exercitationem velit similique fugiat, magnam consectetur nesciunt inventore numquam ducimus doloremque incidunt? Odio possimus quidem officia iusto, obcaecati at saepe. Similique laborum omnis aut id ex quasi molestiae in. Reiciendis rem facilis corrupti quasi temporibus, distinctio magni iusto saepe tempore delectus! Eius exercitationem quasi consequuntur explicabo distinctio atque. Commodi aperiam aliquid fugiat veritatis earum. Tenetur similique corrupti quos atque sunt eligendi, in voluptatem, unde tempore minima amet aspernatur doloribus eum quasi culpa enim itaque, pariatur consequatur deserunt consectetur. Libero ipsam est dolores! Alias odit minus quos iure.</p>
-</div>
-<div>
-
-      <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, delectus adipisci id consequatur exercitationem velit similique fugiat, magnam consectetur nesciunt inventore numquam ducimus doloremque incidunt? Odio possimus quidem officia iusto, obcaecati at saepe. Similique laborum omnis aut id ex quasi molestiae in. Reiciendis rem facilis corrupti quasi temporibus, distinctio magni iusto saepe tempore delectus! Eius exercitationem quasi consequuntur explicabo distinctio atque. Commodi aperiam aliquid fugiat veritatis earum. Tenetur similique corrupti quos atque sunt eligendi, in voluptatem, unde tempore minima amet aspernatur doloribus eum quasi culpa enim itaque, pariatur consequatur deserunt consectetur. Libero ipsam est dolores! Alias odit minus quos iure.</p>
-</div>
 
 
 
